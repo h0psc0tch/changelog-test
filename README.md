5f20cc1b21d80b5dcd730e48dd4da6cc9e8595bd
Greg Scotchburn <greg.scotchburn@dojo.tech>
2022-09-08 11:29:16 +0100

    updated changelog script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # changelog-test
+
+* How to manage hotfix deployments? - just bomb out when not on main?
+* 
---
+++ scripts/git/changelog.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "main" != $(git rev-parse --abbrev-ref HEAD) ]; then
+  echo "cannot generate changelog outside of main"
+  exit
+fi
+
+if [ -z "$1" ]; then
+  echo "no prod SHA supplied"
+  exit
+fi
+
+if [ -z "$2" ]; then
+  echo "no staging SHA supplied"
+  exit
+fi
+
+echo "Commits included in release:"
+git log --ancestry-path $1..$2 --date=format:"%Y-%m-%d %H:%M" --pretty=format:">%ad *%an* <https://github.com/Walkinltd/backend/commit/%H|%s>" | sed 's/"//g'
